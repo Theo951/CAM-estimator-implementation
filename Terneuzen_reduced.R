d00d392c6edd7467df4fbd0868cd3b815bd98314
Theo951 <168425769+Theo951@users.noreply.github.com>
2024-07-17 13:26:50 +0200

    update
    Added phi_0_m adjustment.

--- Terneuzen_reduced.R
+++ Terneuzen_reduced.R
@@ -1,40 +1,23 @@
 library(dplyr)
 library(mice)
 data <- tbc
-# subselect data Using subset function, authors decided to only use 4 columns
-#in order to simplify, I've decided to use their same attributes
+# Simplify the dataset by filtering for the first visit for each patient
+data <- data %>%
+  filter(first == TRUE)
+
+# Retain only the relevant features
 df_subset <- subset(data, select = -c(id,occ,nocc,first,typ,age,ao))
-print("After dropping columns with subset function:")
-print(df_subset)
 
-
-#Generate a complete case scenario as per paper defined
-complete_case <- df_subset [complete.cases(df_subset),]
-#Generate an incomplete case dataset
-incomplete_case <- df_subset[!complete.cases(df_subset),] 
-
+# Check the number of rows with missing values for height_z and bmi_z
 #####
 #105 cases in Am for m = (1, 0, 1)T , where only Y (sex) and X(2) (weight) are observed.
 #####
-subset_data <- df_subset %>%
-  #filter(is.na(bmi.z)&!is.na(hgt.z))
+incomplete_case <- df_subset %>%
   filter(is.na(hgt.z) & is.na(bmi.z))
-  
-
-set.seed(123)
-# Number of observations to extract
-num_samples <- 105
-# Randomly sample row indices
-sample_indices_incomplete <- sample(1:nrow(subset_data), num_samples)
-# Extract the subset
-incomplete_case <- subset_data[sample_indices_incomplete, ]
 
 #We have 201 complete cases in A0
-n_samples <- 201
-sample_indices_complete <- sample(1:nrow(complete_case),n_samples)
-complete_case <- complete_case[sample_indices_complete,]
 
-
+complete_case<- df_subset [complete.cases(df_subset),]
 
 #Statistics of the 2 datasets
 sum(is.na(complete_case))
@@ -44,7 +27,7 @@ summary((incomplete_case))
 
 u_statistics<- function(x){
   #mean(df_subset$wgt.z,na.rm = TRUE)
-  mean(x$bmi.z,na.rm = TRUE)
+  mean(x$bmi.z,na.rm = TRUE) ############ hgt
 }
 
 cam_estimator <- function(complete, incomplete) {
@@ -54,23 +37,27 @@ cam_estimator <- function(complete, incomplete) {
   # Define phi functions
   phi_m_simple <- incomplete_case$wgt.z
   lm_model <- lm(bmi.z ~ wgt.z * sex, data = complete )
+  # Get fitted values for complete cases
+  fitted_values <- fitted(lm_model)
+  phi_0_m <- mean(fitted_values)  # This is phi_0,m
   print(summary(lm_model))
   phi_m_complex <- predict(lm_model, newdata = incomplete)
   print("phi_m_complex")
   print(phi_m_complex)
   
   # Average predictions on incomplete cases
-  #phi_hat_simple <- mean(phi_m_simple, na.rm = TRUE)
+  
   phi_hat_simple <- mean(phi_m_simple, na.rm=TRUE)
-  #phi_hat_complex <- mean(predict(lm(bmi ~ height * sex, 
-  #                                 data = complete), newdata = incomplete), na.rm = TRUE)
   phi_hat_complex <- mean(phi_m_complex,na.rm=TRUE)
   print("phi_hat_complex")
   print( phi_hat_complex)
   
   # Combine estimates
-  cam_simple <- theta_0 - phi_hat_simple #mean(phi_hat_simple)
-  cam_complex <- theta_0 - (phi_hat_complex) #mean(phi_hat_complex)
+  #cam_simple <- theta_0 - phi_hat_simple #mean(phi_hat_simple)
+  #cam_complex <- theta_0 - (phi_hat_complex) #mean(phi_hat_complex)
+  # Combine estimates using phi_0_m
+  cam_simple <- theta_0 + (phi_0_m - phi_hat_simple)
+  cam_complex <- theta_0 + (phi_0_m - phi_hat_complex)
   print("cam_simple")
   print(cam_simple)
   print("cam_complex")
@@ -93,6 +80,8 @@ conf_interval <- function(estimator, data) {
   error <- qnorm(0.975) * se
   lower <- estimator - error
   upper <- estimator + error
+  print(c(lower, upper))
+  
   return(c(lower, upper))
 }
 
@@ -103,9 +92,10 @@ ci_cam_complex <- conf_interval(cam_results$cam_complex, complete_case)
 
 # Calculate interval widths
 width_complete <- ci_complete[2] - ci_complete[1]
+print(width_complete)
 width_cam_simple <- ci_cam_simple[2] - ci_cam_simple[1]
+print(width_cam_simple)
 width_cam_complex <- ci_cam_complex[2] - ci_cam_complex[1]
-
 
 
 
