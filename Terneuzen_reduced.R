7bac26fb28ac18d4af6fabca2bd57add76181022
Theo951 <168425769+Theo951@users.noreply.github.com>
2024-07-18 11:24:03 +0200

    Update Terneuzen_reduced.R

--- Terneuzen_reduced.R
+++ Terneuzen_reduced.R
@@ -74,7 +74,7 @@ cat("CAM estimate (simple): ", cam_results$cam_simple, "\n")
 cat("CAM estimate (complex): ", cam_results$cam_complex, "\n")
 
 # intervals
-conf_interval <- function(estimator, data) {
+conf_interval <- function(estimator, data) { ###HOW TO CALCULATE THE CONFIDENCE INTERVAL OF A PREDICTION?
   n <- nrow(data)
   se <- sd(data$bmi.z, na.rm = TRUE) / sqrt(n)
   error <- qnorm(0.975) * se
@@ -87,6 +87,7 @@ conf_interval <- function(estimator, data) {
 
 # Calculate confidence intervals
 ci_complete <- conf_interval(theta_0, complete_case)
+
 ci_cam_simple <- conf_interval(cam_results$cam_simple, complete_case)
 ci_cam_complex <- conf_interval(cam_results$cam_complex, complete_case)
 
@@ -98,6 +99,112 @@ print(width_cam_simple)
 width_cam_complex <- ci_cam_complex[2] - ci_cam_complex[1]
 
 
+################################################################
+estimate_variance_components_subsample <- function(data, B) {
+  n <- nrow(data)
+  subsample_size <- floor(n / B)
+  
+  psi_U_list <- numeric(B)
+  print("psi_u_list")
+  print(psi_U_list)
+  Omega_U_list <- matrix(0, nrow = B, ncol = 2)
+  Lambda_U_list <- matrix(0, nrow = B, ncol = 2)
+  
+  for (b in 1:B) {
+    subsample <- data[sample(1:n, subsample_size), ]
+    
+    
+    psi_U_list[b] <- var(subsample$bmi.z, na.rm = TRUE)
+    
+    Omega_U_list[b, 1] <- var(subsample$bmi.z, na.rm = TRUE)
+    Omega_U_list[b, 2] <- var(subsample$hgt.z, na.rm = TRUE)
+    Lambda_U_list[b, 1] <- (cov(subsample$bmi.z, subsample$hgt.z)) #na.rm=TRUE
+    Lambda_U_list[b, 2] <- Lambda_U_list[b, 1]
+  }
+  print("Omega_U_list")
+  print(Omega_U_list)
+  print("Lambda_U_list")
+  print(Lambda_U_list)
+  print(paste("Subsample", b))
+  print(subsample)
+  psi_U <- mean(psi_U_list)
+  Omega_U <- apply(Omega_U_list, 2, mean)
+  Lambda_U <- apply(Lambda_U_list, 2, mean)
+  
+  return(list(psi_U = psi_U, Omega_U = Omega_U, Lambda_U = Lambda_U))
+}
 
+conf_interval_subsample <- function(estimator, data, var_components) {
+  n <- nrow(data)
+  psi_U <- var_components$psi_U
+  Omega_U <- var_components$Omega_U
+  Lambda_U <- var_components$Lambda_U
+  
+  # Estimate the variance
+  var_est <- psi_U - Omega_U %*% Lambda_U %*% t(Omega_U)
+  
+  # Standard error
+  se <- sqrt(var_est / n)
+  
+  # Margin of error
+  error <- qnorm(0.975) * se
+  
+  # Confidence interval
+  lower <- estimator - error
+  upper <- estimator + error
+  
+  return(c(lower, upper))
+}
+
+# Example data
+#data <- complete_case
+
+# Number of subsamples
+B <- 105
+
+# Estimate variance components using subsampling
+var_components <- estimate_variance_components_subsample(complete_case, B)
+
+# Example estimator (mean of bmi.z)
+estimator <- mean(complete_case$bmi.z, na.rm = TRUE)
+
+# Calculate the confidence interval using subsamples
+ci <- conf_interval_subsample(estimator, complete_case, var_components)
+
+# Print the confidence interval
+print(ci)
+
+###################################################
+# Function to calculate the confidence interval
+conf_interval_theorem3 <- function(estimator, data, M, psi_U, Omega_U, Lambda_U) {
+  n <- nrow(data)
+  # Estimate the variance
+  var_est <- psi_U - Omega_U %*% Lambda_U %*% t(Omega_U)
+  
+  # Standard error
+  se <- sqrt(var_est / n)
+  
+  # Margin of error
+  error <- qnorm(0.975) * se
+  
+  # Confidence interval
+  lower <- estimator - error
+  upper <- estimator + error
+  
+  return(c(lower, upper))
+}
+
+psi_U <- var(complete_case$bmi.z, na.rm = TRUE)  # This is a placeholder, calculate appropriately
+Omega_U <- matrix(1, nrow=1, ncol=1)  # Placeholder, calculate appropriately
+Lambda_U <- matrix(1, nrow=1, ncol=1)  # Placeholder, calculate appropriately
+
+
+
+n <- nrow(data)
+subsample_size <- floor(n / B)
+for (n in 1:n){                                             #### SUMBSAMPLE ONLY RETURNS 1 ######
+  for (b in 1:B) {
+  subsample <- data[sample(1:n, subsample_size), ]}
+}
 
 
